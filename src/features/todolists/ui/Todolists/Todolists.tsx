51843bab4243ea3d5edddb9cc272f146b35a875c
electrixky <tanyatkachuk2401@gmail.com>
2024-10-26 11:26:06 +0300

    useEffect for getTodolists()

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,16 +1,25 @@
 import { Todolist } from "./Todolist/Todolist"
-import React from "react"
-import { useAppSelector } from "../../../../common/hooks/useAppSelector"
+import React, { useEffect } from "react"
+import { useAppSelector } from "common/hooks/useAppSelector"
 import { selectTodolists } from "../../model/todolistsSelectors"
+import { todolistsApi } from "../../api/todolistsApi"
+import { Simulate } from "react-dom/test-utils"
+import loadedData = Simulate.loadedData
 
 export const Todolists = () => {
   let todolists = useAppSelector(selectTodolists)
 
-  return (
-    <div>
-      {todolists.map((tl) => {
-        return <Todolist key={tl.id} todolist={tl} />
-      })}
-    </div>
-  )
+  useEffect(() => {
+    todolistsApi.getTodolists().then((res) => {
+      const todolists = res.data
+      console.log(todolists)
+    })
+  }, [])
+
+  //   <div>
+  //     {todolists.map((tl) => {
+  //       return <Todolist key={tl.id} todolist={tl} />
+  //     })}
+  //   </div>,
+  // )
 }
